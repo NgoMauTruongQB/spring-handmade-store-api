4a1c8c1ce81a19e02332491e222e3f847672f7ee
NgoMauTruongQB <truongngo2707@gmail.com>
2023-12-22 01:10:14 +0700

    lombok

---
+++ handmade-store/src/main/java/com/nmt/handmadestore/controllers/CustomerGreetingController.java
@@ -0,0 +1,30 @@
+package com.nmt.handmadestore.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nmt.handmadestore.models.Greeting;
+
+
+@RestController
+@RequestMapping("/v1")
+public class CustomerGreetingController {
+    
+    private static final String greetingTemplate = "Hello, %s %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(
+        @RequestParam(defaultValue = "0") boolean gender,
+        @RequestParam(defaultValue = "Customer") String customerName
+    ) {
+        return Greeting.builder()
+            .id(counter.incrementAndGet())
+            .content(greetingTemplate.formatted(gender ? "Mr. " : "Ms. ", customerName))
+            .build();
+    }
+}
---
+++ handmade-store/src/main/java/com/nmt/handmadestore/models/Greeting.java
@@ -0,0 +1,12 @@
+package com.nmt.handmadestore.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Greeting {
+    private long id;
+    private String content;
+
+}
